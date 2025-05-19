d96c6e627ddd022ee33565b73e0d4b89e6e247d3
Caio Gustavo Rodrigues da Silva <caio.rodrigues@cyncly.com>
2025-05-19 18:26:37 -0300

    Student - parcial

---
+++ src/Brainwave.Students.Domain/Student.cs
@@ -0,0 +1,32 @@
+﻿using Brainwave.Core.DomainObjects;
+
+
+namespace Brainwave.Students.Domain
+{
+    class Student : Entity, IAggregateRoot
+    {
+        private readonly List<Enrollment> _enrollments = [];
+        public IReadOnlyCollection<Enrollment> Enrollments => _enrollments;
+
+        public void AdicionarEnrollment(Enrollment Enrollment)
+        {
+            if (EnrollmentExists(Enrollment))
+                throw new DomainException("Matrícula já existente.");
+
+            //TOOD : Verificar se o aluno já está matriculado no curso
+            //TODO:  Pagamento pendente
+
+            _enrollments.Add(Enrollment);
+        }
+
+        public Enrollment? ObterEnrollment(Guid cursoId)
+        {
+            return Enrollments.FirstOrDefault(m => m.CourseId == cursoId && m.StudentId == Id);
+        }
+
+        private bool EnrollmentExists(Enrollment Enrollment)
+        {
+            return Enrollments.Any(m => m.Id == Enrollment.Id);
+        }
+    }
+}
