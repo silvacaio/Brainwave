577baa5d6b222aa2a457074130c8007b8fcecebb
Caio Gustavo Rodrigues da Silva <caio.rodrigues@cyncly.com>
2025-05-28 18:55:42 -0300

    parcial

---
+++ Brainwave.API/Controllers/EnrollmentsController.cs
@@ -0,0 +1,44 @@
+﻿using Brainwave.API.Controllers.Base;
+using Brainwave.API.ViewModel;
+using Brainwave.Core.Messages.CommonMessages.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Brainwave.API.Controllers
+{
+    [Authorize(Roles = "ADMIN,STUDENT")]
+    [Route("api/enrollments")]
+    public class EnrollmentsController : MainController
+    {
+        private readonly IMediator _mediator;
+        private readonly IStudentQueries _studentQueries;
+
+        public EnrollmentsController(INotificationHandler<DomainNotification> notifications,
+                                     IStudentQueries studentQueries,
+                                     IMediator mediator)
+            : base(notifications, mediator)
+        {
+            _mediator = mediator;
+            _studentQueries = studentQueries;
+        }
+
+        [HttpGet("pending-payment")]
+        public async Task<ActionResult<IEnumerable<EnrollmentViewModel>>> GetPendingPaymentEnrollments()
+        {
+            var enrollments = await _studentQueries.GetPendingPaymentEnrollments(UserId);
+            return CustomResponse(enrollments);
+        }
+
+        [HttpPost("{courseId:guid}")]
+        public async Task<IActionResult> Add(Guid courseId)
+        {
+            var command = new AddEnrollmentCommand(UserId, courseId);
+            await _mediator.Send(command);
+
+            return CustomResponse();
+        }
+    }
+
+}
